7ac4416b557d82068456dc08846899a6f8d1fe78
ByteCntrl <bytecntrl@gmail.com>
2023-02-03 20:38:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,73 @@
 #!/usr/bin/python3
+import pathlib
+import re
 import sys
-import os
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.environ.get("_MEIPASS2",os.path.abspath("."))
 
-    return os.path.join(base_path, relative_path)
+def lookup(
+    pci_ids: list[str], 
+    vendor: str, 
+    device: str
+) -> str:
+    check = False
 
-def lookup(vendor, device):
-    pci_ids = open(resource_path('pci.ids')).readlines()
-    ids_lib = {}
     for line in pci_ids:
-        # If line is tabbed or contains an hashtag skips
-        if line[:3] == "\t\t" or line[0] == "#":
-            continue
+        if line[:1].isdigit():
+            if check:
+                break
+            check = line.split()[0] == vendor
 
-        # If line isn't tabbed and isn't a newline create a :
-        if not line[:1] in ['\t', '\n']:
-            temp_vendor = line.split()[0]
-            ids_lib[temp_vendor] = { 'name' : line[6:-1] }
+        if line.startswith('\t') and check:
+            if (desc := line.split(maxsplit=1))[0] == device:
+                return desc[-1].replace("\n", "")
 
-        if line.startswith('\t\t') or line.startswith('\n'):
-            continue
-        if line.startswith('\t'):
-            ids_lib[temp_vendor][line.split()[0]] = {'name': ' '.join(line.split()[1:])}
-    return ids_lib[vendor][device]
-
-if len(sys.argv) < 2:
-    print(f'Wrong usage detected. Usage: ./pcilookup PCIInfo.txt')
-    sys.exit(1)
-
-pciinfo_file = open(sys.argv[1], 'r').readlines()
-loc = int(len(pciinfo_file)/2) + 1
-
-parsed_pciinfo = {}
-
-for x in range(1,loc):
-    parsed_pciinfo[x] = {
-        'vendor_id': '',
-        'device_id': '',
-        'device_path': '',
-        'description': ''}
+    return ""
 
 
-vendor_ids = [line.split()[3][2:-1].lower() for line in pciinfo_file if not line.startswith('   ')]
-device_ids = [line.split()[6][2:-1].lower() for line in pciinfo_file if not line.startswith('   ')]
-device_paths = [line.split()[1] for line in pciinfo_file if line.startswith('   ')]
+def main():
+    if len(sys.argv) < 2:
+        print(f'Wrong usage detected. Usage: ./pcilookup PCIInfo.txt')
+        sys.exit(1)
 
-for x in range(1, loc):
-    parsed_pciinfo[x]['vendor_id'] = vendor_ids[x-1]    
-    parsed_pciinfo[x]['device_id'] = device_ids[x-1]    
-    parsed_pciinfo[x]['device_path'] = device_paths[x-1]    
-    try:
-        parsed_pciinfo[x]['description'] = lookup(vendor=vendor_ids[x-1], device=device_ids[x-1])['name']
-    except:
-        parsed_pciinfo[x]['description'] = 'No data available'    
+    path = pathlib.Path(sys.argv[1])
+    path_pci_ids = pathlib.Path(".") / "pci.ids"
+
+    if not path.exists() or not path.is_file():
+        print(f"{path.name} not exist or isn't a file")
+        sys.exit(1)
+    
+    if not path_pci_ids.exists():
+        print("pci.ids not exist")
+        sys.exit(1)    
+
+    with open(path, 'r') as f:
+        pciinfo_file = f.read()
+
+    with open(path_pci_ids, 'r') as f:
+        pci_ids = f.readlines()
+
+    pci_list = [
+        re.sub(r"\s+", " ", x.strip())
+        for x in re.split(r"\d+\.", pciinfo_file.replace("\n", ""))
+        if x
+    ]
+    parsed_pciinfo = {}
+
+    for i, pci in enumerate(pci_list):
+        vendor_id = re.search(r"Vendor ID: 0[xX]([0-9a-fA-F]+),", pci).group(1).lower()
+        device_id = re.search(r"Device ID: 0[xX]([0-9a-fA-F]+),", pci).group(1).lower()
+        device_path = re.search(r"DevicePath: ([a-zA-Z0-9\(\)\/,]+)", pci).group(1)
+
+        parsed_pciinfo[i + 1] = {
+            "vendor_id": vendor_id,
+            "device_id": device_id,
+            "device_path": device_path,
+            "description": lookup(pci_ids, vendor_id, device_id)
+        }
+
+    for k, v in parsed_pciinfo.items():
+        print(f"{k}: {v['description']}\n\tVendor ID: {v['vendor_id']}\n\tDevice ID: {v['device_id']}\n\tDevice Path: {v['device_path']}")
 
 
-for k,v in parsed_pciinfo.items():
-    print(f"{k}: {v['description']}\n\tVendor ID: {v['vendor_id']}\n\tDevice ID: {v['device_id']}\n\tDevice Path: {v['device_path']}")
+if __name__ == "__main__":
+    main()
