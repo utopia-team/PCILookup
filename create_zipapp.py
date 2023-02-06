5bac227853017cdfdbe204597a137131ad3403d0
ByteCntrl <bytecntrl@gmail.com>
2023-02-06 23:03:51 +0100

    Update create_zipapp.py and remove src folder

--- create_zipapp.py
+++ create_zipapp.py
@@ -1,8 +1,20 @@
+import pathlib
 import zipapp
 
 
+FILES = (
+    ('main.py',),
+    ('pci.ids',)
+)
+
+
 def main():
-    zipapp.create_archive("src", "PCILookup.pyz", main="main:main")
+    zipapp.create_archive(
+        ".", 
+        "PCILookup.pyz", 
+        main="main:main", 
+        filter=lambda x: x.parts in FILES
+    )
 
 
 if __name__ == "__main__":
